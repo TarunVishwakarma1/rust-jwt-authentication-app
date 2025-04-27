fb0827602d2516fa8cb0653b909f96ab8d87bf5c
Tarun Vishwakarma <tarunvishwakarma81@gmail.com>
2025-04-28 00:06:55 +0530

    added get_token_handler: Now the get token returns the token for the passed email

--- axum-auth/src/main.rs
+++ axum-auth/src/main.rs
@@ -1,4 +1,4 @@
-use axum::{http::{header, StatusCode}, response::Response, routing::{get, post}, Json, Router};
+use axum::{http::{header, Response, StatusCode}, routing::{get, post}, Json, Router};
 use serde_json::json;
 use tokio::net::TcpListener;
 use jwt_lib::User;
@@ -33,5 +33,30 @@ async fn public_view_handler() -> Response<String>{
 }
 
 async fn get_token_handler(Json(user): Json<User>) -> Response<String> {
+    let token = jwt_lib::get_jwt(user);
+    match token {
+        Ok(token) => Response::builder()
+            .status(StatusCode::OK)
+            .header(header::CONTENT_TYPE, "application/json")
+            .body(json!(
+                {
+                    "success": true,
+                    "data": {
+                        "token": token
+                    }
+                }
+            ).to_string()).unwrap_or_default(),
 
+        Err(err) => Response::builder()
+        .status(StatusCode::BAD_REQUEST)
+        .header(header::CONTENT_TYPE, "application/json")
+        .body(json!(
+            {
+           "status":false,
+           "data": {
+            "message": String::from("Error Occurred: ")+ &err
+           }
+        }
+        ).to_string()).unwrap_or_default()
+    }
 }
--- jwt-lib/src/lib.rs
+++ jwt-lib/src/lib.rs
@@ -1,5 +1,5 @@
 use chrono::{Utc, Duration};
-use jsonwebtoken::{encode, Header, Encoding};
+use jsonwebtoken::{encode, Header, EncodingKey};
 use serde::{Deserialize, Serialize};
 
 #[derive(Deserialize, Debug, Serialize)]
@@ -7,14 +7,22 @@ pub struct User {
     pub email: String,
 }
 
+#[derive(Deserialize, Serialize, Debug)]
 struct Claims {
     pub email: String,
     pub exp: i64,
 }
 
-pub fn get_jwt(user: User){
-    let token = encode(&Header::default(), &Claims{
-        email: user.email,
-        exp: (Utc::now() + Duration::minutes(1)).timestamp(),
-    },&Encoding::from_secret("mykey".as_bytes()));
+pub fn get_jwt(user: User) -> Result<String, String>{
+    let token = encode(
+        &Header::default(),
+        &Claims {
+          email: user.email,
+          exp: (Utc::now() + Duration::minutes(1)).timestamp(),
+        },
+        &EncodingKey::from_secret("mykey".as_bytes()),
+      )
+      .map_err(|e| e.to_string());
+    
+      return token;
 }
