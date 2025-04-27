08a50aedcde713b5646c46d2121bd9ac887a091f
Tarun Vishwakarma <tarunvishwakarma81@gmail.com>
2025-04-28 00:48:46 +0530

    completed axum-api

--- axum-auth/src/main.rs
+++ axum-auth/src/main.rs
@@ -1,14 +1,15 @@
-use axum::{http::{header, Response, StatusCode}, routing::{get, post}, Json, Router};
+use axum::{extract::FromRequestParts, http::{header, request::Parts, Response, StatusCode}, routing::{get, post}, Json, Router};
 use serde_json::json;
 use tokio::net::TcpListener;
 use jwt_lib::User;
+use async_trait::async_trait;
 
 #[tokio::main(flavor = "multi_thread", worker_threads = 4)]
 async fn main() {
     let routes = Router::new()
         .route("/public-view", get(public_view_handler))
         .route("/get-token", post(get_token_handler))
-        .route("/secret-view", get(|| async {"secret view"}));
+        .route("/secret-view", get(secret_view_handler));
 
     let tcp_listner = TcpListener::bind("127.0.0.1:8080")
         .await
@@ -59,4 +60,78 @@ async fn get_token_handler(Json(user): Json<User>) -> Response<String> {
         }
         ).to_string()).unwrap_or_default()
     }
+}
+
+async fn secret_view_handler(Auth(user): Auth) -> Response<String> {
+    Response::builder()
+    .status(StatusCode::OK)
+    .header(header::CONTENT_TYPE, "application/json")
+    .body(
+      json!({
+        "success": true,
+        "data": user
+      })
+      .to_string(),
+    )
+    .unwrap_or_default()
+}
+
+struct Auth(User);
+
+#[async_trait]
+impl<S> FromRequestParts<S> for Auth
+where
+  S: Send + Sync,
+{
+  type Rejection = Response<String>;
+
+  async fn from_request_parts(parts: &mut Parts, _: &S) -> Result<Self, Self::Rejection> {
+    let access_token = parts
+      .headers
+      .get(header::AUTHORIZATION)
+      .and_then(|value| value.to_str().ok())
+      .and_then(|str| str.split(" ").nth(1));
+
+    match access_token {
+      Some(token) => {
+        let user = jwt_lib::decode_jwt(token);
+
+        match user {
+          Ok(user) => Ok(Auth(user)),
+
+          Err(e) => Err(
+            Response::builder()
+              .status(StatusCode::UNAUTHORIZED)
+              .header(header::CONTENT_TYPE, "application/json")
+              .body(
+                json!({
+                  "success": false,
+                  "data": {
+                    "message": e
+                  }
+                })
+                .to_string(),
+              )
+              .unwrap_or_default(),
+          ),
+        }
+      }
+
+      None => Err(
+        Response::builder()
+          .status(StatusCode::UNAUTHORIZED)
+          .header(header::CONTENT_TYPE, "application/json")
+          .body(
+            json!({
+              "success": false,
+              "data": {
+                "message": "No token provided"
+              }
+            })
+            .to_string(),
+          )
+          .unwrap_or_default(),
+      ),
+    }
+  }
 }
--- jwt-lib/src/lib.rs
+++ jwt-lib/src/lib.rs
@@ -1,5 +1,5 @@
 use chrono::{Utc, Duration};
-use jsonwebtoken::{encode, Header, EncodingKey};
+use jsonwebtoken::{decode, encode, DecodingKey, EncodingKey, Header, Validation};
 use serde::{Deserialize, Serialize};
 
 #[derive(Deserialize, Debug, Serialize)]
@@ -25,4 +25,17 @@ pub fn get_jwt(user: User) -> Result<String, String>{
       .map_err(|e| e.to_string());
     
       return token;
+}
+
+pub fn decode_jwt(token: &str) -> Result<User, String> {
+    let token_data = decode::<User>(
+            token, 
+        &DecodingKey::from_secret("mykey".as_bytes()), 
+            &Validation::default()
+        );
+
+    match token_data {
+        Ok(token_data) => Ok(token_data.claims),
+        Err(e) => Err(e.to_string())
+    }
 }
