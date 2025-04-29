d3a1a765b8b8c0715c1dc1c5a55fedc64a0ac518
Tarun Vishwakarma <tarunvishwakarma81@gmail.com>
2025-04-29 21:35:55 +0530

    Added Actix routes

--- actix-auth/src/main.rs
+++ actix-auth/src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{web::{get, post}, App, HttpServer};
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(move || {
+        App::new()
+            .route("/public-view", get().to( || async {"Public View"}))
+            .route("/get-token", post().to(|| async {"Get Token"}))
+            .route("/secret-view", get().to(|| async {"Secret View"}))
+    })
+        .workers(4)
+        .bind("127.0.0.1:2424")
+        .expect("Address should be free and valid")
+        .run()
+        .await
 }
